2c0312829598b65670aab51d85c4341265d4f080
jhonnykos <krylova.oa@mail.ru>
2022-02-08 16:18:17 +0300

    add redux form

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import Main from "./components/MainComponent";
 import './App.css';
 import {Component} from "react";
 import {BrowserRouter} from "react-router-dom"
+import {Provider} from "react-redux"; //чтобы стор был доступен в любом компоненте
+import {ConfigureStore} from "./redux/configureStore";
+
+const store = ConfigureStore();
 
 class App extends Component{
     render() {
         return (
-            <BrowserRouter>
-                <div>
-                    <Main/>
-                </div>
-            </BrowserRouter>
+            <Provider store={store}>
+                <BrowserRouter>
+                    <div>
+                        <Main/>
+                    </div>
+                </BrowserRouter>
+            </Provider>
         );
     }
 }
