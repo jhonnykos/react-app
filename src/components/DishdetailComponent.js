4bb24360387becef76f28897eb1945fdade80cd9
jhonnykos <krylova.oa@mail.ru>
2022-03-14 11:11:03 +0300

    add fetch and fetch errors

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -13,11 +13,13 @@ import {
 import {Link} from "react-router-dom";
 import {Control, Errors, LocalForm} from "react-redux-form";
 import {Loading} from "./LoadingComponen";
+import {baseUrl} from "../shared/baseUrl";
+
 
 function RenderDish({dish}) {
     return (
         <Card>
-            <CardImg width="100%" src={dish.image} alt={dish.name}/>
+            <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name}/>
             <CardBody>
                 <CardTitle>{dish.name}</CardTitle>
                 <CardText>{dish.description}</CardText>
